621d17622ea0bfa477604a255402dd67edda07b0
Jumabek <jumabek4044@gmail.com>
2024-08-30 20:28:40 +0900

    include conf in fps experiment output

--- tracking/run_fps.py
+++ tracking/run_fps.py
@@ -19,7 +19,7 @@ def process_sequence(seq_path, args, gpu_id=0):
     tick = time.time()
 
     print(f'Processing sequence {seq_path} on {device}', flush=True)
-    
+
     args.source = seq_path
     args.device = device
     args.yolo_model = "yolov8m"
@@ -34,9 +34,7 @@ def process_sequence(seq_path, args, gpu_id=0):
         print(f'Creating project directory {args.project}', flush=True)
         os.makedirs(args.project)
 
-
     run(args)
-
 
     tock = time.time()
 
@@ -46,20 +44,21 @@ def process_sequence(seq_path, args, gpu_id=0):
     time_spent_for_the_sequence = tock - tick
 
     avg_time_per_frame = (time_spent_for_the_sequence) / num_frames
-    
+
     FPS = 1/avg_time_per_frame
-  
+
     path_to_fps_csv = f'results/{os.path.basename(args.dataset)}-FPS/{args.sequence}/fps.csv'
-   
+
     if not os.path.exists(path_to_fps_csv):
         with open(path_to_fps_csv, 'w') as f:
             f.write('tracker_name,sequence_name,FPS\n')
-            
+
     with open(path_to_fps_csv, 'a') as f:
         if args.appearance_feature_layer is None:
-            f.write(f'{args.tracking_method},{seq_name},{FPS:.1f}\n')
+            f.write(f'{args.tracking_method},{seq_name},{FPS:.1f},{args.conf}\n')
         else:
-            f.write(f'{"LITE" + args.tracking_method},{seq_name},{FPS:.1f}\n')
+            f.write(
+                f'{"LITE" + args.tracking_method},{seq_name},{FPS:.1f},{args.conf}\n')
 
     print(
         f'Finished processing sequence {seq_path} on {device} in {tock - tick:.2f} seconds', flush=True)
@@ -73,7 +72,7 @@ if __name__ == '__main__':
 
     # Ensure the source is a directory containing subdirectories with image sequences
     if os.path.isdir(args.dataset):
-        seq_dir = os.path.join(args.dataset, args.split, args.sequence, 'img1') 
+        seq_dir = os.path.join(args.dataset, args.split, args.sequence, 'img1')
     else:
         raise ValueError("The provided source path is not a directory")
 
